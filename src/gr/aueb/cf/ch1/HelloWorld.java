5056c16f92cd7c1271a552e4d329e0725cea2867
ThanouCh <thn.xristina@gmail.com>
2024-12-10 20:01:25 +0200

    Initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints Hello Coding Factory in
+ * standard output (console)
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Coding Factory!");  //Prints Hello Coding Factory
+        System.out.print("Hello World");
+        System.out.println();
+    }
+}
